2c1e72d359dec75b54b10c0a84ba6c88fecf83bc
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-24 15:43:34 +0100

    Added other infos to the dice-roll exercise

--- js/dice-roll.js
+++ js/dice-roll.js
@@ -1,6 +1,14 @@
 /* --------------------------------------------------------------------
 GENERIC INITIALIZATION AND DECLARATION */
 
+// * INITIALIZATION
+
+// ? TAKING FROM THE DOM
+
+const diceRollSection = document.querySelector('#dice-roll');
+const btnRoll = document.querySelector('a.my-btn');
+const btnRefresh = document.querySelector('a.my-btn.d-none');
+
 
 
 
@@ -9,3 +17,10 @@ GENERIC INITIALIZATION AND DECLARATION */
 MAIN CODE */
 
 // * LINEAR PROGRAMM
+
+
+
+// * EVENTS
+btnRoll.addEventListener('click',function(){
+    const pElement = document.createElement('p');
+})
